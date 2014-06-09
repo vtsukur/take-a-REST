ac16629ef4eae285afb1f36f129864f47ebeafed
volodymyr.tsukur <volodymyr.tsukur@avid.com>
2014-06-09 23:18:36 +0300

    Creating sample resource with HAL support. Providing HAL support for JAX-RS 2.0. Integrating HAL Builder API into:  - https://github.com/HalBuilder  - http://gotohal.net

---
+++ src/main/java/org/realrest/infrastructure/rest/jaxrs/HalMessageBodyWriter.java
@@ -0,0 +1,48 @@
+package org.realrest.infrastructure.rest.jaxrs;
+
+import com.theoryinpractise.halbuilder.api.ReadableRepresentation;
+import com.theoryinpractise.halbuilder.api.RepresentationFactory;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.MessageBodyWriter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+/**
+ * Based on the implementation of
+ * <a href="https://github.com/HalBuilder/halbuilder-jaxrs/blob/develop/src/main/java/com/theoryinpractise/halbuilder/jaxrs/JaxRsHalBuilderSupport.java">com.theoryinpractise.halbuilder.jaxrs.JaxRsHalBuilderSupport</a>.
+ *
+ * @author volodymyr.tsukur
+ */
+@Provider
+public class HalMessageBodyWriter implements MessageBodyWriter {
+
+    private static final MediaType HAL_JSON_MEDIA_TYPE = MediaType.valueOf(RepresentationFactory.HAL_JSON);
+
+    @Override
+    public boolean isWriteable(final Class type, final Type genericType, final Annotation[] annotations,
+            final MediaType mediaType) {
+        return ReadableRepresentation.class.isAssignableFrom(type) && mediaType.isCompatible(HAL_JSON_MEDIA_TYPE);
+    }
+
+    @Override
+    public long getSize(final Object t, final Class type, final Type genericType, final Annotation[] annotations,
+            final MediaType mediaType) {
+        return -1;
+    }
+
+    @Override
+    public void writeTo(final Object t, final Class type, final Type genericType, final Annotation[] annotations,
+            final MediaType mediaType, final MultivaluedMap httpHeaders, final OutputStream entityStream)
+            throws IOException, WebApplicationException {
+        final ReadableRepresentation representation = (ReadableRepresentation) t;
+        representation.toString(mediaType.toString(), new OutputStreamWriter(entityStream));
+    }
+
+}
--- src/main/java/org/realrest/infrastructure/rest/jaxrs/HelloResource.java
+++ src/main/java/org/realrest/infrastructure/rest/jaxrs/HelloResource.java
@@ -1,5 +1,9 @@
 package org.realrest.infrastructure.rest.jaxrs;
 
+import com.theoryinpractise.halbuilder.api.Representation;
+import com.theoryinpractise.halbuilder.api.RepresentationFactory;
+import com.theoryinpractise.halbuilder.standard.StandardRepresentationFactory;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -14,6 +18,15 @@ public class HelloResource {
     @Produces("text/plain")
     public String world() {
         return "world";
+    }
+
+    @GET
+    @Path("hal")
+    @Produces(RepresentationFactory.HAL_JSON)
+    public Representation representation() {
+        final RepresentationFactory factory = new StandardRepresentationFactory();
+        factory.withFlag(RepresentationFactory.PRETTY_PRINT);
+        return factory.newRepresentation().withProperty("key", "value");
     }
 
 }
