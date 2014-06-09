54e759f0a7de2c13a4ea22833fc00ffa564bd033
flushdia <flushdia@gmail.com>
2014-06-09 23:32:46 +0300

    Adding entry point resource and stub /hotels resource.

---
+++ src/main/java/org/realrest/infrastructure/rest/jaxrs/HotelsResource.java
@@ -0,0 +1,23 @@
+package org.realrest.infrastructure.rest.jaxrs;
+
+import com.theoryinpractise.halbuilder.api.Representation;
+import com.theoryinpractise.halbuilder.api.RepresentationFactory;
+import com.theoryinpractise.halbuilder.standard.StandardRepresentationFactory;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@Path("hotels")
+public final class HotelsResource {
+
+    @GET
+    @Produces(RepresentationFactory.HAL_JSON)
+    public Representation retrieve() {
+        return new StandardRepresentationFactory().newRepresentation();
+    }
+
+}
