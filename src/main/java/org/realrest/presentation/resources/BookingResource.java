9e7acb8871f4b06e5c6d34a10a1d25d74004a0a6
volodymyr.tsukur <volodymyr.tsukur@avid.com>
2014-06-22 23:14:05 +0300

    Renaming and restructuring routine for booking representation building: avoiding statics.

--- src/main/java/org/realrest/presentation/resources/BookingResource.java
+++ src/main/java/org/realrest/presentation/resources/BookingResource.java
@@ -3,7 +3,7 @@ package org.realrest.presentation.resources;
 import com.google.code.siren4j.component.Entity;
 import org.realrest.application.service.BookingService;
 import org.realrest.domain.BookingNotFoundException;
-import org.realrest.presentation.representations.BookingRepresentation;
+import org.realrest.presentation.representations.BookingRepresentationBuilder;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
@@ -19,12 +19,15 @@ public class BookingResource {
     @Inject
     private BookingService bookingService;
 
+    @Inject
+    private BookingRepresentationBuilder bookingRepresentationBuilder;
+
     @GET
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Entity read(@PathParam("id") final Long id, @Context final UriInfo uriInfo) {
         try {
-            return BookingRepresentation.fromBooking(bookingService.findById(id));
+            return bookingRepresentationBuilder.prepare(bookingService.findById(id));
         }
         catch (final BookingNotFoundException e) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
