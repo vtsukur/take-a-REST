97d30e91bcfe6c80f8c93787d2b5cf74e047a856
flushdia <flushdia@gmail.com>
2014-06-22 14:29:45 +0300

    Introducing serialization and deserialization for Java 8 date/time instances. Extending test to supply date values.

---
+++ src/main/java/org/realrest/infrastructure/rest/jaxrs/serialization/CustomObjectMapperContextResolver.java
@@ -0,0 +1,32 @@
+package org.realrest.infrastructure.rest.jaxrs.serialization;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class CustomObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+
+    private final ObjectMapper mapper;
+
+    public CustomObjectMapperContextResolver() {
+        mapper = new ObjectMapper();
+        mapper.setAnnotationIntrospector(new JacksonAnnotationIntrospector());
+        mapper.registerModule(new JSR310Module());
+    }
+
+    @Override
+    public ObjectMapper getContext(final Class<?> type) {
+        return mapper;
+    }
+
+}
