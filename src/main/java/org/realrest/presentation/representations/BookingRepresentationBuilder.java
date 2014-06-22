4cc3429c121f2820a8e6a31f770d0dc1da7927d1
flushdia <flushdia@gmail.com>
2014-06-22 23:14:05 +0300

    Renaming and restructuring routine for booking representation building: avoiding statics.

--- src/main/java/org/realrest/presentation/representations/BookingRepresentationBuilder.java
+++ src/main/java/org/realrest/presentation/representations/BookingRepresentationBuilder.java
@@ -4,12 +4,15 @@ import com.google.code.siren4j.component.Entity;
 import com.google.code.siren4j.component.builder.EntityBuilder;
 import org.realrest.domain.Booking;
 
+import javax.enterprise.context.ApplicationScoped;
+
 /**
  * @author volodymyr.tsukur
  */
-public interface BookingRepresentation {
+@ApplicationScoped
+public class BookingRepresentationBuilder {
 
-    public static Entity fromBooking(final Booking booking) {
+    public Entity prepare(final Booking booking) {
         return EntityBuilder.newInstance().
                 setComponentClass("booking").
                 addProperty("id", booking.getId()).
