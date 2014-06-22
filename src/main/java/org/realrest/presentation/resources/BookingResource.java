9318d3d4e2e5b7c4d7d8c1a12a10b9bb7ce78ef5
volodymyr.tsukur <volodymyr.tsukur@avid.com>
2014-06-22 23:29:36 +0300

    Moving URI building procedure for booking to BookingRepresentationBuilder. Renaming BookingRepresentationBuilder.prepare() method to build() so that *Builder class name makes more sense ;)

--- src/main/java/org/realrest/presentation/resources/BookingResource.java
+++ src/main/java/org/realrest/presentation/resources/BookingResource.java
@@ -2,13 +2,13 @@ package org.realrest.presentation.resources;
 
 import com.google.code.siren4j.component.Entity;
 import org.realrest.application.service.BookingService;
+import org.realrest.domain.Booking;
 import org.realrest.domain.BookingNotFoundException;
 import org.realrest.presentation.representations.BookingRepresentationBuilder;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
-import java.net.URI;
 
 /**
  * @author volodymyr.tsukur
@@ -27,18 +27,13 @@ public class BookingResource {
     @Produces(MediaType.APPLICATION_JSON)
     public Entity read(@PathParam("id") final Long id, @Context final UriInfo uriInfo) {
         try {
-            return bookingRepresentationBuilder.prepare(bookingService.findById(id));
+            final Booking booking = bookingService.findById(id);
+            final UriBuilder uriBuilder = uriInfo.getBaseUriBuilder();
+            return bookingRepresentationBuilder.build(booking, uriBuilder);
         }
         catch (final BookingNotFoundException e) {
             throw new WebApplicationException(Response.Status.NOT_FOUND);
         }
-    }
-
-    public static URI readURI(final UriBuilder uriBuilder, final Long id) {
-        return uriBuilder.
-                path(BookingResource.class).
-                path(id.toString()).
-                build();
     }
 
 }
--- src/main/java/org/realrest/presentation/resources/BookingsResource.java
+++ src/main/java/org/realrest/presentation/resources/BookingsResource.java
@@ -2,6 +2,7 @@ package org.realrest.presentation.resources;
 
 import org.realrest.domain.Booking;
 import org.realrest.application.service.BookingService;
+import org.realrest.presentation.representations.BookingRepresentationBuilder;
 import org.realrest.presentation.transitions.CreateBookingTransition;
 
 import javax.inject.Inject;
@@ -21,12 +22,15 @@ public class BookingsResource {
     @Inject
     private BookingService bookingService;
 
+    @Inject
+    private BookingRepresentationBuilder bookingRepresentationBuilder;
+
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public Response create(final CreateBookingTransition data, @Context final UriInfo uriInfo) {
         final Booking result = bookingService.create(data);
-        final URI bookingURI = BookingResource.readURI(uriInfo.getBaseUriBuilder(), result.getId());
+        final URI bookingURI = bookingRepresentationBuilder.buildURI(result, uriInfo.getBaseUriBuilder());
         return Response.created(bookingURI).build();
     }
 
