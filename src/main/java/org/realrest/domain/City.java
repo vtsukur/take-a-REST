929a7cb88876e44d0f23a74d927cc8b561b57b8c
flushdia <flushdia@gmail.com>
2014-06-29 22:07:21 +0300

    Introducing City associated with the Hotel.

--- src/main/java/org/realrest/domain/City.java
+++ src/main/java/org/realrest/domain/City.java
@@ -3,19 +3,13 @@ package org.realrest.domain;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Set;
-
 /**
  * @author volodymyr.tsukur
  */
 @Getter
 @Setter
-public final class Hotel extends Identifiable {
-
-    private City city;
+public class City extends Identifiable {
 
     private String name;
-
-    private Set<Room> rooms;
 
 }
