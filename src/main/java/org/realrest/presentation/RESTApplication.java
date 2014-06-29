c2c9fc158fa5957fcd62ebfbc6ce4ab5dd33e3d8
flushdia <flushdia@gmail.com>
2014-06-29 18:44:41 +0300

    Getting rid of web.xml.

--- src/main/java/org/realrest/presentation/RESTApplication.java
+++ src/main/java/org/realrest/presentation/RESTApplication.java
@@ -5,6 +5,7 @@ import org.realrest.presentation.resources.BookingsResource;
 import org.realrest.presentation.resources.EntryPointResource;
 import org.realrest.presentation.resources.HotelsResource;
 
+import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 import java.util.HashSet;
 import java.util.Set;
@@ -12,6 +13,7 @@ import java.util.Set;
 /**
  * @author volodymyr.tsukur
  */
+@ApplicationPath("/api")
 public class RESTApplication extends Application {
 
     private final Set<Class<?>> classes = new HashSet<>();
