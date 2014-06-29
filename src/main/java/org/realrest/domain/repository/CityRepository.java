ce0d2b8ef73495bb9b8f2fd06724a0deec871692
glvtsuku <volodymyr.tsukur@avid.com>
2014-06-29 22:24:39 +0300

    Adding CityRepository.

---
+++ src/main/java/org/realrest/domain/repository/CityRepository.java
@@ -0,0 +1,10 @@
+package org.realrest.domain.repository;
+
+import org.realrest.domain.City;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public interface CityRepository extends BaseRepository<City> {
+
+}
