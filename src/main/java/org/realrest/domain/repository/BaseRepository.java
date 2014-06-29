361619c0f10bc73fa295894edbbf311bc5038e49
glvtsuku <volodymyr.tsukur@avid.com>
2014-06-29 21:30:26 +0300

    Adding Hotel service and repository.

--- src/main/java/org/realrest/domain/repository/BaseRepository.java
+++ src/main/java/org/realrest/domain/repository/BaseRepository.java
@@ -3,6 +3,8 @@ package org.realrest.domain.repository;
 import org.realrest.domain.EntityNotFoundException;
 import org.realrest.domain.Identifiable;
 
+import java.util.Collection;
+
 /**
  * @author volodymyr.tsukur
  */
@@ -11,6 +13,8 @@ public interface BaseRepository<E extends Identifiable> {
     E create(E data);
 
     E findById(Long id) throws EntityNotFoundException;
+
+    Collection<E> findAll();
 
     void delete(Long id);
 
---
+++ src/main/java/org/realrest/domain/repository/impl/InMemoryHotelRepository.java
@@ -0,0 +1,18 @@
+package org.realrest.domain.repository.impl;
+
+import org.realrest.domain.Hotel;
+import org.realrest.domain.repository.HotelRepository;
+
+import javax.enterprise.context.ApplicationScoped;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@ApplicationScoped
+public class InMemoryHotelRepository extends BaseInMemoryBookingRepository<Hotel> implements HotelRepository {
+
+    public InMemoryHotelRepository() {
+        super("Hotel");
+    }
+
+}
