303c482e88a5f303efd2f603a8eba4361c89c27f
glvtsuku <volodymyr.tsukur@avid.com>
2014-06-29 21:59:43 +0300

    Restoring approach with web.xml to bring back application to the working state.

--- src/main/java/org/realrest/presentation/RESTApplication.java
+++ src/main/java/org/realrest/presentation/RESTApplication.java
@@ -1,12 +1,32 @@
 package org.realrest.presentation;
 
+import org.realrest.presentation.resources.BookingResource;
+import org.realrest.presentation.resources.BookingsResource;
+import org.realrest.presentation.resources.EntryPointResource;
+import org.realrest.presentation.resources.HotelsResource;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author volodymyr.tsukur
  */
 @ApplicationPath("/api")
 public class RESTApplication extends Application {
+
+    private final Set<Class<?>> classes = new HashSet<>();
+
+    public RESTApplication() {
+        classes.add(EntryPointResource.class);
+        classes.add(HotelsResource.class);
+        classes.add(BookingsResource.class);
+        classes.add(BookingResource.class);
+    }
+
+    public Set<Class<?>> getClasses() {
+        return classes;
+    }
 
 }
