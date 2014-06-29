8c61319d4d58e8c49c173312fdc1dcafb2999efb
glvtsuku <volodymyr.tsukur@avid.com>
2014-06-29 18:44:41 +0300

    Getting rid of web.xml.

---
+++ src/main/java/org/realrest/infrastructure/cdi/weld/listener/WeldListener.java
@@ -0,0 +1,13 @@
+package org.realrest.infrastructure.cdi.weld.listener;
+
+import org.jboss.weld.environment.servlet.Listener;
+
+import javax.servlet.annotation.WebListener;
+
+/**
+ * @author volodymyr.tsukur
+ */
+@WebListener
+public class WeldListener extends Listener {
+
+}
