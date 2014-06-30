476c199087e9538f28ff3ac2966375d131aa95d8
glvtsuku <volodymyr.tsukur@avid.com>
2014-06-30 17:44:58 +0300

    Adding offset and limit to representation.

--- src/main/java/org/realrest/presentation/representations/HotelsRepresentationBuilder.java
+++ src/main/java/org/realrest/presentation/representations/HotelsRepresentationBuilder.java
@@ -4,6 +4,7 @@ import com.google.code.siren4j.component.Entity;
 import com.google.code.siren4j.component.Link;
 import com.google.code.siren4j.component.builder.EntityBuilder;
 import com.google.code.siren4j.component.builder.LinkBuilder;
+import org.realrest.application.service.Pagination;
 import org.realrest.domain.Hotel;
 import org.realrest.presentation.resources.HotelsResource;
 
@@ -20,16 +21,23 @@ public class HotelsRepresentationBuilder {
 
     private final Collection<Hotel> hotels;
 
+    private final Pagination pagination;
+
     private final UriInfo uriInfo;
 
-    public HotelsRepresentationBuilder(final Collection<Hotel> hotels, final UriInfo uriInfo) {
+    public HotelsRepresentationBuilder(final Collection<Hotel> hotels,
+                                       final Pagination pagination,
+                                       final UriInfo uriInfo) {
         this.hotels = hotels;
+        this.pagination = pagination;
         this.uriInfo = uriInfo;
     }
 
     public Entity build() {
         return EntityBuilder.newInstance().
                 setComponentClass("hotels").
+                addProperty("offset", pagination.getOffset()).
+                addProperty("limit", pagination.getLimit()).
                 addSubEntities(hotels()).
                 addLink(LinkBuilder.newInstance().
                         setHref(selfHref()).
