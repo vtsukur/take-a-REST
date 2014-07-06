0638c2b6c4bfd309455ba7be4d62925387af2716
glvtsuku <volodymyr.tsukur@avid.com>
2014-07-06 22:27:19 +0300

    Moving CustomObjectMapperContextResolver to a 'jaxrs' package.

--- src/main/java/org/letustakearest/infrastructure/jaxrs/CustomObjectMapperContextResolver.java
+++ src/main/java/org/letustakearest/infrastructure/jaxrs/CustomObjectMapperContextResolver.java
@@ -1,4 +1,4 @@
-package org.letustakearest.infrastructure.providers;
+package org.letustakearest.infrastructure.jaxrs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
