bffcfaa60b1e290593fc0e93c0c6fd25bbfde103
flushdia <flushdia@gmail.com>
2014-07-06 20:57:42 +0300

    Utility to simplify creation of entity tags from integers.

---
+++ src/main/java/org/letustakearest/presentation/cache/EntityTagFactory.java
@@ -0,0 +1,18 @@
+package org.letustakearest.presentation.cache;
+
+import javax.ws.rs.core.EntityTag;
+
+/**
+ * @author volodymyr.tsukur
+ */
+public final class EntityTagFactory {
+
+    private EntityTagFactory() {
+
+    }
+
+    public static EntityTag entityTag(int value) {
+        return new EntityTag(String.valueOf(value));
+    }
+
+}
