43e9b0de550753e1a6f616fd07228d63372e42f5
flushdia <flushdia@gmail.com>
2014-07-16 00:25:01 +0300

    Adding link to an error.

--- src/main/java/org/letustakearest/infrastructure/jaxrs/ValidationExceptionMapper.java
+++ src/main/java/org/letustakearest/infrastructure/jaxrs/ValidationExceptionMapper.java
@@ -2,7 +2,9 @@ package org.letustakearest.infrastructure.jaxrs;
 
 import org.letustakearest.application.service.ValidationException;
 
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
@@ -12,12 +14,16 @@ import javax.ws.rs.ext.Provider;
 @Provider
 public class ValidationExceptionMapper implements ExceptionMapper<ValidationException> {
 
+    @Context
+    private UriInfo uriInfo;
+
     @Override
     public Response toResponse(final ValidationException exception) {
         final Error error = new Error();
         error.setType(exception.getClass().getSimpleName());
         error.setMessage(exception.getMessage());
         error.setAction(exception.getAction());
+        error.setLink(uriInfo.getBaseUriBuilder().segment("error", exception.getAction()).build().toString());
         return Response.
                 status(Response.Status.BAD_REQUEST).
                 entity(error).
