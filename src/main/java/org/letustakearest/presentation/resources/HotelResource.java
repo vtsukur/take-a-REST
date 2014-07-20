b1bf055019a9a3ee5701dd6a7d5bdf709c88647e
glvtsuku <volodymyr.tsukur@avid.com>
2014-07-20 20:56:51 +0300

    Sample of runtime content negotiation.

--- src/main/java/org/letustakearest/presentation/resources/HotelResource.java
+++ src/main/java/org/letustakearest/presentation/resources/HotelResource.java
@@ -5,16 +5,15 @@ import com.google.code.siren4j.component.Entity;
 import org.letustakearest.application.service.HotelService;
 import org.letustakearest.domain.EntityNotFoundException;
 import org.letustakearest.domain.Hotel;
-import org.letustakearest.presentation.representations.HotelWithPlacesRepresentationBuilder;
 import org.letustakearest.presentation.representations.HotelRepresentationBuilder;
+import org.letustakearest.presentation.representations.HotelWithPlacesRepresentationBuilder;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.core.*;
+import java.util.List;
 
 /**
  * @author volodymyr.tsukur
@@ -41,6 +40,24 @@ public class HotelResource {
     @Produces({ "application/vnd.siren.hotel.v2+json" })
     public Entity readWithPlaces(@Context final UriInfo uriInfo) {
         return prepareHotelAsPlaceRepresentation(uriInfo);
+    }
+
+    @GET
+    @Path("/runtime-content-negotiation")
+    public Response readViaRuntimeContentNegotiation(
+            @Context final UriInfo uriInfo,
+            @Context final Request request) {
+        final List<Variant> variants = Variant.mediaTypes(
+                MediaType.valueOf(Siren4J.JSON_MEDIATYPE),
+                MediaType.valueOf("application/vnd.siren.hotel.v2+json")
+        ).build();
+        final Variant variant = request.selectVariant(variants);
+        if (variant == null) {
+            return Response.notAcceptable(variants).build();
+        }
+        else {
+            return Response.ok(prepareHotelAsPlaceRepresentation(uriInfo), variant).build();
+        }
     }
 
     @GET
