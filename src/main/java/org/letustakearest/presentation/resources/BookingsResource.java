360b6ed7f86bf39ae918c62dffec303aa9aeb884
flushdia <flushdia@gmail.com>
2014-08-27 00:32:34 +0300

    Reworking BookingResource and BookingsResource to use ResourceContext.

--- src/main/java/org/letustakearest/presentation/resources/BookingsResource.java
+++ src/main/java/org/letustakearest/presentation/resources/BookingsResource.java
@@ -6,17 +6,14 @@ import org.letustakearest.application.service.BookingService;
 import org.letustakearest.domain.Booking;
 import org.letustakearest.domain.EntityNotFoundException;
 import org.letustakearest.presentation.cache.CacheControlFactory;
-import org.letustakearest.presentation.representations.BookingRepresentationAssembler;
 import org.letustakearest.presentation.representations.BookingsRepresentationAssembler;
 import org.letustakearest.presentation.representations.cdi.SelectByAcceptHeader;
 import org.letustakearest.presentation.transitions.CreateBookingAsPlaceTransition;
-import org.letustakearest.presentation.transitions.SetBookingTransition;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.container.ResourceContext;
 import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import java.net.URI;
@@ -37,10 +34,6 @@ public class BookingsResource {
     @SelectByAcceptHeader
     private BookingsRepresentationAssembler bookingsRepresentationAssembler;
 
-    @Inject
-    @SelectByAcceptHeader
-    private BookingRepresentationAssembler bookingRepresentationAssembler;
-
     @GET
     @Produces({ RepresentationFactory.HAL_JSON, Siren4J.JSON_MEDIATYPE })
     public Response browse() {
@@ -48,20 +41,6 @@ public class BookingsResource {
                 ok(bookingsRepresentationAssembler.from(bookingService.findAll())).
                 cacheControl(CacheControlFactory.oneMinute()).
                 build();
-    }
-
-    @POST
-    @Produces({ RepresentationFactory.HAL_JSON, Siren4J.JSON_MEDIATYPE })
-    @Consumes(MediaType.APPLICATION_JSON)
-    public Response create(final SetBookingTransition transition, @Context final UriInfo uriInfo) {
-        final Booking result;
-        try {
-            result = bookingService.create(transition);
-        } catch (EntityNotFoundException e) {
-            throw new WebApplicationException(Response.Status.BAD_REQUEST);
-        }
-        final URI bookingURI = BookingResource.selfURI(result, uriInfo);
-        return Response.created(bookingURI).build();
     }
 
     @POST
